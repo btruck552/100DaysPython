66246a3b06e4fc6d567cf25722cd7b42a5691936
btruck552 <51246614+btruck552@users.noreply.github.com>
2019-06-03 15:30:03 -0400

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 15:08:44 2019
+
+@author: btruck552
+"""
+
+print(10+6)
+
+print(10 - 6)
+
+print(10 * 6)
+
+print(10 / 6)
+
+print(10 // 6)
+
+print(10 ** 6)
+
+print(10 % 6)
+
+print(8 * (2 + 2) + 3 ** 2 + 1)
+
+print(10 // 6)
+print(10 // 6.0)
+print(10 // 6.00)
+
+print("Python" + ": It's a fun language to learn")
+
+print("\"This is a quote.\"")
+print("C:\\newFolder")
+
+print("That's right. Python is cool.")
+
+print("""This is a 
+block quote """)
+
+print("Rudy " * 5)
+
+print("This doesn't have to stay\non one line")
+
+print("\tThe test can also be indented|")
+
+print("C:\newFolder")
+
+print(r"C:\newFolder")
