f64b7bfa7832a1fc2f2ab9d43bc06535326e5ec0
btruck552 <51246614+btruck552@users.noreply.github.com>
2019-06-04 12:48:25 -0400

    Add files via upload

---
+++ Module1/Day5.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+cheers = 'where everybody knows YOUR name'
+
+print(cheers.capitalize())
+
+print(cheers.title())
+print(cheers.lower())
+print(cheers.upper())
+print(cheers.swapcase())
+print("norm".upper())
+
+spinoff = 'Fraiser'
+yr = 1993
+print("The show {} was a spinoff of Cheers that first aired in {}".format(spinoff,yr))
+
+i = 13
+
+print("{0:2} squared is {1:4}.".format(i,i ** 2))
+
+print("{0:2} squared is {1:<4}.".format(i,i ** 2))
+print("{0:2} squared is {1:>4}.".format(i,i ** 2))
+print("{0:2} squared is {1:^4}.".format(i,i ** 2))
+
+msg = "END OF CODE"
+print("{:=^50}".format(msg))
+
+print("{:.5}".format(cheers.capitalize()))
+
+pi = 22 / 7
+print("Pi as a float is {0:f}, as a float with a precision of 2 is {0:.2f}".format(pi))
+print("The answer to life, the universe, and everything is {0:d} as an integer, or {0:=^10d} as a padded and centered integer".format(42))
+
+print(f"Pi as a float is {pi:f}, as a float with a precision of 2 is {pi:.2f}")
+print(f"The answer to life, the universe, and everything is {42:d} as an integer, or {42:=^10d} as a padded and centered integer")
