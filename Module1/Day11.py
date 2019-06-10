05496cf354ed58fdc0c26202f8362030025540e2
btruck552 <51246614+btruck552@users.noreply.github.com>
2019-06-10 09:19:18 -0400

    Add files via upload

---
+++ Module1/Day11.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  6 08:49:04 2019
+
+@author: btruck552
+"""
+
+x = 42
+x += 3
+print(x) 
+
+x = 42
+x -= 3
+print(x)
+
+x = 42
+x *= 3
+print(x)
+
+x = 42
+x /= 3
+print(x)
+
+x = 42
+x //= 3
+print(x)
+
+x = 42
+x %= 3
+print(x)
+
+x = 42
+x **= 3
+print(x)
+
+x = 1
+x <<= 2
+print(x)
+
+x = 4
+x >>= 1
+print(x)
+
+x = True
+y = False
+x &= y
+print(x)
+
+x = True
+y = False
+x ^= y
+print(x)
+
+x = True
+y = False
+x |= y
+print(x)
+
+x = "Ten Million "
+y = "Dollars"
+x *= 3
+x += y
+print(x)
